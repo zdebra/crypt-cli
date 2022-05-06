1a2c2cc13c81e55feac1a70010550d3a5eceb0fd
zdebra <zd.brabec@gmail.com>
2022-05-06 19:29:30 +0200

    commands added

--- main.go
+++ main.go
@@ -1,5 +1,64 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var encryptCmd = &cobra.Command{
+	Use:   "encrypt",
+	Short: "Encrypt text with password",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println(args)
+		if len(args) < 1 {
+			return fmt.Errorf("no text to encrypt provided")
+		}
+		plaintext := strings.Join(args, " ")
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter password: ")
+		password, _ := reader.ReadString('\n')
+
+		encrypted, err := Encrypt(password, plaintext)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("Encrypted message:", encrypted)
+		return nil
+	},
+}
+
+var decryptCmd = &cobra.Command{
+	Use:   "decrypt",
+	Short: "Decrypt text with password",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return fmt.Errorf("no text to decrypt provided")
+		}
+		encrypted := strings.Join(args, " ")
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter password: ")
+		password, _ := reader.ReadString('\n')
+
+		plaintext, err := Decrypt(password, encrypted)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("Decrypted message:", plaintext)
+		return nil
+	},
+}
+
 func main() {
-	
+	var rootCmd = &cobra.Command{Use: "app"}
+	rootCmd.AddCommand(encryptCmd, decryptCmd)
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
